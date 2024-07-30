43deb845c8ea5fdccf3ac534c2be24316ad67094
duque-dev <ali.duque.dev@gmail.com>
2024-07-29 19:00:13 -0500

    project structure

---
+++ applications/app-service/src/main/java/co/com/book/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package co.com.book.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "co.com.book.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
